d632127ff508d92446a6566d8667e091245df8ea
MattSeb15 <mateosebastian159@gmail.com>
2023-11-26 19:24:48 -0500

    Adding blocks and recipes

--- src/main/java/net/most/survivaltimemod/item/ModCreativeModTabs.java
+++ src/main/java/net/most/survivaltimemod/item/ModCreativeModTabs.java
@@ -5,9 +5,11 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
+import net.most.survivaltimemod.block.ModBlocks;
 
 public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
@@ -20,6 +22,9 @@ public class ModCreativeModTabs {
                             .displayItems((pParameters, pOutput) -> {
                                 for (RegistryObject<Item> item : ModItems.ITEMS_LIST) {
                                     pOutput.accept(item.get());
+                                }
+                                for(RegistryObject<Block> block : ModBlocks.BLOCK_LIST) {
+                                    pOutput.accept(block.get());
                                 }
                             })
                             .build());
