16e91e3f5e60257c1278141d2b657ee226a04ba7
MattSeb15 <mateosebastian159@gmail.com>
2023-11-29 00:05:11 -0500

    Adding ores, raw, raw_block, loot_tables. TODO: ind ingots, blocks->shards

--- src/main/java/net/most/survivaltimemod/block/ModBlocks.java
+++ src/main/java/net/most/survivaltimemod/block/ModBlocks.java
@@ -1,9 +1,12 @@
 package net.most.survivaltimemod.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,7 +24,32 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, SurvivalTimeMod.MOD_ID);
 
     public static final RegistryObject<Block> OPAL_BLOCK = registerBlock("opal_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)
+                    .mapColor(DyeColor.PURPLE)
+                    .strength(15.0f, 2000.0f)
+                    .sound(SoundType.AMETHYST)));
+    public static final RegistryObject<Block> OPAL_RAW_BLOCK = registerBlock("opal_raw_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)
+                    .mapColor(DyeColor.MAGENTA)
+                    .strength(10.0f, 1800.0f)
+                    .sound(SoundType.CORAL_BLOCK)));
+    // Ores
+    public static final RegistryObject<Block> OPAL_ORE = registerBlock("opal_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(5.0f, 10.0f)
+                    .requiresCorrectToolForDrops(), UniformInt.of(3, 9)));
+    public static final RegistryObject<Block> DEEPSLATE_OPAL_ORE = registerBlock("deepslate_opal_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE)
+                    .strength(6.0f, 30.0f)
+                    .requiresCorrectToolForDrops(), UniformInt.of(5, 9)));
+    public static final RegistryObject<Block> NETHER_OPAL_ORE = registerBlock("nether_opal_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHERRACK)
+                    .strength(4.0f, 20.0f)
+                    .requiresCorrectToolForDrops(), UniformInt.of(6, 10)));
+    public static final RegistryObject<Block> END_STONE_OPAL_ORE = registerBlock("end_stone_opal_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)
+                    .strength(8.0f, 100.0f)
+                    .requiresCorrectToolForDrops(), UniformInt.of(8, 10)));
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
@@ -35,7 +63,13 @@ public class ModBlocks {
     }
 
     public static List<RegistryObject<Block>> BLOCK_LIST = List.of(
-            OPAL_BLOCK
+            OPAL_BLOCK,
+            OPAL_RAW_BLOCK,
+            //Ores
+            OPAL_ORE,
+            DEEPSLATE_OPAL_ORE,
+            NETHER_OPAL_ORE,
+            END_STONE_OPAL_ORE
     );
 
     public static void register(IEventBus eventBus) {
--- src/main/java/net/most/survivaltimemod/item/ModItems.java
+++ src/main/java/net/most/survivaltimemod/item/ModItems.java
@@ -17,18 +17,21 @@ public class ModItems {
             ITEMS.register("opal", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> OPAL_SHARD_CHRONA =
             ITEMS.register("opal_shard_chrona", () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Item> OPAL_SHARD_TEMPORA =
             ITEMS.register("opal_shard_tempora", () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Item> OPAL_SHARD_EPOCH =
             ITEMS.register("opal_shard_epoch", () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Item> OPAL_SHARD_FLUX =
             ITEMS.register("opal_shard_flux", () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Item> OPAL_SHARD_LOOP =
             ITEMS.register("opal_shard_loop", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> OPAL_RAW =
+            ITEMS.register("opal_raw", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> OPAL_INGOT =
+            ITEMS.register("opal_ingot", () -> new Item(new Item.Properties()));
+    //TODO: Añadir un item que al ser usado, te consuma tiempo y se convierta en algo cuando lo haga y sea para
+    // craftear algo.
+
 
     public static final List<RegistryObject<Item>> ITEMS_LIST = List.of(
             OPAL,
@@ -36,7 +39,9 @@ public class ModItems {
             OPAL_SHARD_TEMPORA,
             OPAL_SHARD_EPOCH,
             OPAL_SHARD_FLUX,
-            OPAL_SHARD_LOOP
+            OPAL_SHARD_LOOP,
+            OPAL_RAW,
+            OPAL_INGOT
     );
 
     public static void register(IEventBus eventBus) {
