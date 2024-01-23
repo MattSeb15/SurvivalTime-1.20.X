d26b1b04a2132e0c22f81fc898f9524a27fe2a59
MattSeb15 <mateosebastian159@gmail.com>
2024-01-23 10:24:20 -0500

    AXE, PICKAXE, HOE AND SHOVEL: MODELS AND TEXTURES

---
+++ src/main/java/net/most/survivaltimemod/util/records/HoePattern.java
@@ -0,0 +1,12 @@
+package net.most.survivaltimemod.util.records;
+
+import net.minecraft.world.level.ItemLike;
+
+public class HoePattern extends ToolPattern {
+
+
+    public HoePattern(ItemLike result, int craftTime, int energyCost, ItemLike stickIngredient, ItemLike bladeIngredient,
+                      ItemLike shardIngredient) {
+        super(result, craftTime, energyCost, stickIngredient, bladeIngredient, shardIngredient);
+    }
+}
